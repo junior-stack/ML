04d1ef5b3e5e4f6b68404996d6149071b6080abe
qianding <bruce,qian@mail.utoronto.ca>
2020-12-12 21:13:10 -0500

    starter code

--- starter_code/part_a/ensemble.py
+++ starter_code/part_a/ensemble.py
@@ -8,19 +8,24 @@ def bootstrap():
     train_dic = load_train_csv("../data")
     l = []
     for i in range(3):
+        size = len(train_dic["user_id"])
+        user = []
+        question = []
+        correct = []
         m = np.zeros([542, 1774]) * np.nan
-        user = train_dic["user_id"]
-        question = train_dic["question_id"]
-        correct = train_dic["is_correct"]
-        random.seed(i)
-        random.shuffle(user)
-        random.seed(i)
-        random.shuffle(question)
-        random.seed(i)
-        random.shuffle(correct)
         for i in range(30000):
-            m[user[i]][question[i]] = correct[i]
-        l.append(m)
-    return l[0],l[1],l[2]
+            idx = random.randint(0,size-1)
+            u = train_dic["user_id"][idx]
+            user.append(u)
+            q = train_dic["question_id"][idx]
+            question.append(q)
+            c = train_dic["is_correct"][idx]
+            correct.append(c)
+            m[u][q] += c
+        train_d = {"user_id":user,"question_id":question,"is_correct":correct}
+        return train_d, m
+
+a,b = bootstrap()
+print(len(a["user_id"]))
 
 
